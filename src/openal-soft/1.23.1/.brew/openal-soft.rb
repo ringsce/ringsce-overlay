525c3a4ca8e9ac70a51fc17dbe5ebc93af232b6a
Pedro Dias Vicente (Gleentech Solutions) <pedrolvicente@gmail.com>
2024-05-26 14:37:45 +0100

    first commit

---
+++ src/openal-soft/1.23.1/.brew/openal-soft.rb
@@ -0,0 +1,48 @@
+class OpenalSoft < Formula
+  desc "Implementation of the OpenAL 3D audio API"
+  homepage "https://openal-soft.org/"
+  url "https://openal-soft.org/openal-releases/openal-soft-1.23.1.tar.bz2"
+  sha256 "796f4b89134c4e57270b7f0d755f0fa3435b90da437b745160a49bd41c845b21"
+  license "LGPL-2.0-or-later"
+  head "https://github.com/kcat/openal-soft.git", branch: "master"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?openal-soft[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  keg_only :shadowed_by_macos, "macOS provides OpenAL.framework"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+
+  def install
+    # Please don't re-enable example building. See:
+    # https://github.com/Homebrew/homebrew/issues/38274
+    args = %w[
+      -DALSOFT_BACKEND_PORTAUDIO=OFF
+      -DALSOFT_BACKEND_PULSEAUDIO=OFF
+      -DALSOFT_EXAMPLES=OFF
+      -DALSOFT_MIDI_FLUIDSYNTH=OFF
+    ]
+    args << "-DCMAKE_INSTALL_RPATH=#{rpath}"
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include "AL/al.h"
+      #include "AL/alc.h"
+      int main() {
+        ALCdevice *device;
+        device = alcOpenDevice(0);
+        alcCloseDevice(device);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lopenal"
+  end
+end
