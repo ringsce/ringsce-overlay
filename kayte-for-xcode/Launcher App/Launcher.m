3b9735e6618e0c24a2aa35ef51b4868621f908f2
Pedro Dias Vicente (Gleentech Solutions) <pedrolvicente@gmail.com>
2024-06-01 10:57:15 +0100

    fixing cmake for D

--- kayte-for-xcode/Launcher App/Launcher.m
+++ kayte-for-xcode/Launcher App/Launcher.m
@@ -1,24 +1,22 @@
 /**
- * @file main.c
- * @brief D for Xcode Launcher
+ * @file Launcher.m
+ * @brief Kayte for Xcode Launcher
  *
- * D for Xcode is free software; you can redistribute it and/or modify it
+ * Kayte for Xcode is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option)
  * any later version.
  *
- * D for Xcode is distributed in the hope that it will be useful, but WITHOUT
+ * Kayte for Xcode is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
  * more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with D for Xcode; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA.
  *
- * This launcher program responds to open and print documents AppleEvents
+ * This launcher program responds to open and print document AppleEvents
  * by passing them to Xcode.
  */
+
 
 #include <ApplicationServices/ApplicationServices.h>
 #include <Carbon/Carbon.h>
